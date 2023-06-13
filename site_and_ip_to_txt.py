cb09fdf7dfa5b5d78972acb3604e43ecd0962725
j35se <jesse@j35se.com>
2023-06-13 12:27:04 -0400

    Update site_and_ip_to_txt.py
    Added a try and except block.

--- site_and_ip_to_txt.py
+++ site_and_ip_to_txt.py
@@ -1,9 +1,13 @@
+# Add error handeling
+
+
+
 from datetime import datetime
 import socket as s
 
 list_of_site = []
 list_of_ip = []
-currentday = datetime.now().replace(second=0, microsecond=0)
+currentday = datetime.now().replace(microsecond=0)
 
 def get_ip_addr():
     while True:
@@ -12,10 +16,14 @@ def get_ip_addr():
         if site == "q":
             break
         else:
-            ip = s.gethostbyname(site)
-            print(f"The IP for {site} is {ip}.")
-            list_of_site.append(site)
-            list_of_ip.append(ip)
+            try:
+                ip = s.gethostbyname(site)
+                print(f"The IP for {site} is {ip}.")
+                list_of_site.append(site)
+                list_of_ip.append(ip)
+            except:
+                print("Sorry. That site didn't work.")
+                continue
 
 def write_ip_to_list():
     with open("Websites and IPs.txt", 'w') as file:
@@ -24,7 +32,7 @@ def write_ip_to_list():
             file.write(f'{list_of_site[-1]}:  {list_of_ip[-1]}\n')
             list_of_ip.pop(-1)
             list_of_site.pop(-1)
-        
+    print('The websites and IPs have been saved to a text file.')
 
 def main():
     get_ip_addr()
