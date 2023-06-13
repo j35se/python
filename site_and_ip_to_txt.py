5255514eb15985d0e203f6bafa64caa92272c6b8
j35se <jesse@j35se.com>
2023-06-13 12:09:45 -0400

    Create site_and_ip_to_txt.py
    Initial commit

---
+++ site_and_ip_to_txt.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import socket as s
+
+list_of_site = []
+list_of_ip = []
+currentday = datetime.now().replace(second=0, microsecond=0)
+
+def get_ip_addr():
+    while True:
+        site = input("\nEnter another site or 'q' to exit: ")
+        site = site.lower()
+        if site == "q":
+            break
+        else:
+            ip = s.gethostbyname(site)
+            print(f"The IP for {site} is {ip}.")
+            list_of_site.append(site)
+            list_of_ip.append(ip)
+
+def write_ip_to_list():
+    with open("Websites and IPs.txt", 'w') as file:
+        file.write(f'This file was created on {currentday}\n')
+        for i in range(len(list_of_ip)):                  
+            file.write(f'{list_of_site[-1]}:  {list_of_ip[-1]}\n')
+            list_of_ip.pop(-1)
+            list_of_site.pop(-1)
+        
+
+def main():
+    get_ip_addr()
+    write_ip_to_list()
+    
+    
+main()
